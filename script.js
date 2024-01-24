4022ef1ccd744fa6f7c753bb070e057662682ec0
Koala Code <k0a1ac0d3@gmail.com>
2024-01-24 15:39:52 +0100

    Improved: -js.

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
 $(document).ready(function () {
-    let language = 'en'; // Default language
     let translations;
 
     // Load translations from JSON file
-    $.ajax({
-        url: 'translations.json',
-        async: false,
-        dataType: 'json',
-        success: function (data) {
-            translations = data;
-        }
+    $.getJSON('translations.json', function (data) {
+        translations = data;
+
+        // Get the browser's language
+        let browserLanguage = navigator.language.substring(0, 2);
+
+        // If translations for the browser language exist, use them; otherwise, fallback to 'en' (English)
+        language = translations[browserLanguage] ? browserLanguage : 'en';
+
+        // Call the function to display random numbers when the page is loaded
+        displayRandomNumbers();
+    }).fail(function () {
+        console.error('Failed to load translations. Please check the translations.json file.');
     });
 
-    let $sortableList = $('#sortable-list');
+    const $sortableList = $('#sortable-list');
     let gameWon = false;
 
     // Function to generate a random circle
     function generateRandomCircle() {
-        let circle = $('<div class="circle"></div>');
-        let size = Math.floor(Math.random() * (100 - 20 + 1)) + 20;
-        let x = Math.random() * (window.innerWidth - size);
-        let y = Math.random() * (window.innerHeight - size);
-        let color = Math.random() < 0.5 ? '#eadcff' : '#ffecdc';
+        const circle = $('<div class="circle"></div>');
+        const size = Math.floor(Math.random() * (100 - 20 + 1)) + 20;
+        const x = Math.random() * (window.innerWidth - size);
+        const y = Math.random() * (window.innerHeight - size);
+        const color = Math.random() < 0.5 ? '#eadcff' : '#ffecdc';
 
         circle.css({ left: x + 'px', top: y + 'px', width: size + 'px', height: size + 'px', background: color });
         $('body').append(circle);
@@ -38,39 +43,59 @@ $(document).ready(function () {
 
     // Function to display random numbers on the page
     function displayRandomNumbers() {
-        let numbers = generateRandomNumbers();
+        // Remove existing circles
+        $('.circle').remove();
+
+        const numbers = generateRandomNumbers();
         $sortableList.empty().append(numbers.map(value => `<li>${value}</li>`));
 
         // Make list items draggable only if the game is not won
         if (!gameWon) {
-            $sortableList.find('li').draggable({
-                revert: 'invalid', cursor: 'grab', zIndex: 100, helper: 'original',
-                start: function () { $(this).addClass('grabbed'); },
-                stop: function () { $(this).removeClass('grabbed'); }
-            }).droppable({
-                accept: 'li', tolerance: 'pointer',
-                over: function () { $(this).addClass('over'); },
-                out: function () { $(this).removeClass('over'); },
-                drop: function (event, ui) {
-                    let dropped = ui.helper, dropIndex = $(this).index();
-                    dropIndex === $sortableList.find('li').length - 1
-                        ? dropped.insertAfter($sortableList.find('li').eq(dropIndex))
-                        : dropped.insertBefore($sortableList.find('li').eq(dropIndex));
-                    dropped.removeAttr('style');
-                    checkOrder();
-                    correctlyOrdered() && $sortableList.find('li').draggable('destroy').droppable('destroy');
-                    $(this).removeClass('over');
-                }
-            });
+            makeListItemsDraggable();
         }
 
-        // Get the browser's language
-        let browserLanguage = navigator.language.substring(0, 2);
-
-        // If translations for the browser language exist, use them; otherwise, fallback to 'en' (English)
-        language = translations[browserLanguage] ? browserLanguage : 'en';
-
         // Update messages based on the selected language
+        updateMessages();
+
+        // Check order and display messages
+        checkOrder();
+    }
+
+    // Function to make list items draggable
+    function makeListItemsDraggable() {
+        $sortableList.find('li').draggable({
+            revert: 'invalid', cursor: 'grab', zIndex: 100, helper: 'original',
+            start: function () { $(this).addClass('grabbed'); },
+            stop: function () { $(this).removeClass('grabbed'); }
+        }).droppable({
+            accept: 'li', tolerance: 'pointer',
+            over: function () { $(this).addClass('over'); },
+            out: function () { $(this).removeClass('over'); },
+            drop: handleDrop
+        });
+    }
+
+    // Function to handle drop events
+    function handleDrop(event, ui) {
+        const dropped = ui.helper;
+        const dropIndex = $(this).index();
+
+        dropIndex === $sortableList.find('li').length - 1
+            ? dropped.insertAfter($sortableList.find('li').eq(dropIndex))
+            : dropped.insertBefore($sortableList.find('li').eq(dropIndex));
+
+        dropped.removeAttr('style');
+        checkOrder();
+
+        if (correctlyOrdered()) {
+            destroyDragAndDrop();
+        }
+
+        $(this).removeClass('over');
+    }
+
+    // Function to update messages based on language
+    function updateMessages() {
         $('#message').text(translations[language].message);
         $('#restartBtn').text(translations[language].restartBtn);
         $('h1').text(translations[language].dragToSort);
@@ -80,11 +105,11 @@ $(document).ready(function () {
 
     // Function to check if the numbers are in the correct order
     function checkOrder() {
-        let orderedNumbers = $sortableList.find('li').map(function () {
+        const orderedNumbers = $sortableList.find('li').map(function () {
             return parseInt($(this).text(), 10);
         }).get();
 
-        let correctlyOrdered = orderedNumbers.slice().sort((a, b) => a - b).toString() === orderedNumbers.toString();
+        const correctlyOrdered = orderedNumbers.slice().sort((a, b) => a - b).toString() === orderedNumbers.toString();
 
         // Display the corresponding message
         if (correctlyOrdered) {
@@ -97,7 +122,7 @@ $(document).ready(function () {
 
     // Function to check if the numbers are correctly ordered
     function correctlyOrdered() {
-        let orderedNumbers = $sortableList.find('li').map(function () {
+        const orderedNumbers = $sortableList.find('li').map(function () {
             return parseInt($(this).text(), 10);
         }).get();
 
@@ -105,8 +130,11 @@ $(document).ready(function () {
         return orderedNumbers.every((num, i, arr) => i === 0 || num >= arr[i - 1]);
     }
 
-    // Call the function to display random numbers when the page is loaded
-    displayRandomNumbers();
+    // Function to destroy draggable and droppable properties
+    function destroyDragAndDrop() {
+        $sortableList.find('li').draggable('destroy');
+        $sortableList.find('li').droppable('destroy');
+    }
 
     // Event handler to reset the game state
     $('#restartBtn').on('click', function () {
@@ -115,4 +143,5 @@ $(document).ready(function () {
         displayRandomNumbers();
         $('#message, #restartBtn').hide();
     });
+
 });
